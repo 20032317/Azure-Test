c2b2fc548445253a6e2753de62be6c06df07c80a
20032317 <20032317@mydbs.ie>
2024-07-04 10:33:06 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello Batman!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")  # URL leading to method
+def hello():  # Name of the method
+    return "Hello World!"  # Indent this line
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='8080')  # Indent this line
